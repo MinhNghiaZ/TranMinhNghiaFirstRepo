dc45900f699812d26c1d20807c4c2f2a5e73e41e
Nghia <>
2024-11-29 14:27:21 +0700

    v3

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Tran Minh Nghia Java Repository.");
         System.out.println("Enter your name: ");
-        String yourname = sc.next();
-        System.out.println("Hello "+ yourname);
-        System.out.println("Welcome to my repository");
         
-
+        
     }
 }
