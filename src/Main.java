b6086cd65b477511083165647e17582a472dd8b6
Nghia <>
2024-11-29 14:10:08 +0700

    update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Tran Minh Nghia Java Repository.");
+        System.out.println("Enter your name: ");
+        String yourname = sc.next();
+        System.out.println("Hello "+ yourname);
+        System.out.println("Welcome to my repository");
+        
+
     }
 }
